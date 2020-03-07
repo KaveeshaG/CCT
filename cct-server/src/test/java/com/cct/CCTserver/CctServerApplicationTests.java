c28ace51f737c3d0fe602229133f2cb9191a4b53
bawantharathnayakasliit <bawantharathnayaka@gmail.com>
2020-03-07 23:17:40 +0530

    config spring boot

---
+++ cct-server/src/test/java/com/cct/CCTserver/CctServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cct.CCTserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

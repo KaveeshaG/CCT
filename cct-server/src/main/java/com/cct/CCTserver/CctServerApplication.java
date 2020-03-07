b156b2fd8b5f3380047d1aeb62da4668636b9b7d
bawantharathnayakasliit <bawantharathnayaka@gmail.com>
2020-03-07 22:56:14 +0530

    config spring boot

---
+++ cct-server/src/main/java/com/cct/CCTserver/CctServerApplication.java
@@ -0,0 +1,13 @@
+package com.cct.CCTserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctServerApplication.class, args);
+	}
+
+}
